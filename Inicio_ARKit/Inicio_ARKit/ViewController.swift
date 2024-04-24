69ebdfe18e2c6650de45f97523f81dd0c94e78fb
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-04-24 19:05:17 +0200

    Add files via upload

---
+++ Inicio_ARKit/Inicio_ARKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Inicio_ARKit
+//
+//  Created by dam2 on 24/4/24.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Configurar opciones y ejecutar el sceneView
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+    }
+}
+
