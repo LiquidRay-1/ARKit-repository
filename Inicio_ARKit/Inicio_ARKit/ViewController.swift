6bb77aa31b0f90df24324c1b7e0eafc02373b66a
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-04-24 19:35:25 +0200

    Segunda esfera
    Se ha añadido una segunda esfera que aparece en el plano un metro por detras teniendo como referencia la primera esfera creada (que no es esfera)

--- Inicio_ARKit/Inicio_ARKit/ViewController.swift
+++ Inicio_ARKit/Inicio_ARKit/ViewController.swift
@@ -23,11 +23,16 @@ class ViewController: UIViewController {
     
     @IBAction func dibujar(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.3, height: 0.05, length: 0.3, chamferRadius: 0.3)
+        node.geometry = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 3)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
         node.position = SCNVector3(0, 0, -1)
         self.sceneView.scene.rootNode.addChildNode(node)
+        
+        let node2 = SCNNode()
+        node2.geometry = SCNSphere(radius: 0.15)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        node2.position = SCNVector3(1, 0, -1)
+        node.addChildNode(node2)
     }
     
 }
-
